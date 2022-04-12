3b0a0a21d20a29b6addf464423f4c3476fbdac3a
Ehsan Qader <ehs9nino@gmail.com>
2022-04-12 18:34:12 +0500

    ESL linter fix

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-s
+const bars = document.querySelector('.bars');
+const navMenu = document.querySelector('nav-menu');
+bars.addEventListener('click', () => {
+  bars.classList.toggle('active');
+  navMenu.classList.toggle('active');
+});
