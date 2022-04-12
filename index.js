baedf0c491d8bc56c3454d2de18984a480af9f3c
Precious Akams <preciousakams@yahoo.com>
2022-04-12 15:05:51 +0100

    update

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const bars = document.querySelector('.bars');
-const navMenu = document.querySelector('nav-menu');
+const navMenu = document.querySelector('.nav-menu');
 bars.addEventListener('click', () => {
   bars.classList.toggle('active');
   navMenu.classList.toggle('active');
